054ca1e88f3a329aba4008484b97da5035654cca
wheejoo <53321214+wheejoo@users.noreply.github.com>
2019-08-20 13:25:36 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+
+// 1
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// 2
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+// 3안녕
+app.post('/df', function(request, response) {
+    var agent = new WebhookClient({ request, response });
+    const {intent,query} = agent;
+    console.log ({intent,query});
+  
+    function testAct (agent) {
+      agent.add('서버에서 출력');
+    }
+  
+    let intentMap = new Map();
+    intentMap.set('This is a snack!', testAct);
+    agent.handleRequest(intentMap);
+});
+
+
